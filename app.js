a0baa9955d2f802614db6758818a20e8cf98c331
jvincentbasto <jvincentbasto@gmail.com>
2026-02-12 15:25:27 +0800

    setup app - jwt, mongodb and common core pacakges

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-'use strict'
+import autoload from "@fastify/autoload";
+import { join } from "node:path";
 
-const path = require('node:path')
-const AutoLoad = require('@fastify/autoload')
+export default async function (fastify, opts) {
+  await fastify.register(autoload, {
+    dir: join(process.cwd(), "src/plugins"),
+  });
 
-// Pass --options via CLI arguments in command to enable these options.
-const options = {}
-
-module.exports = async function (fastify, opts) {
-  // Place here your custom code!
-
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
+  await fastify.register(autoload, {
+    dir: join(process.cwd(), "src/routes"),
+    options: { prefix: "/api" },
+  });
 }
 
-module.exports.options = options
+export const options = {
+  port: process.env.PORT ?? 3000,
+  host: process.env.HOST ?? "127.0.0.1",
+  logger: {
+    level: "info",
+  },
+  // connectionTimeout: 30_000,
+  // requestTimeout: 60_000,
+};
